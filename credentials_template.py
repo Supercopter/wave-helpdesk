c01b1a89a8d88633dd61d42c1c4064f790422717
nat.abbotts <nat.abbotts@4f5216a3-65fd-0ba5-d8bf-7ce06e0d288a>
2010-06-28 11:15:27 +0000

    Added: credentials_template.py Contains variable names for the credentials referenced in the code. Useful info about the credentials included.

---
+++ credentials_template.py
@@ -0,0 +1,17 @@
+# Template for the credentials module
+# TODO Rename this file to "credentials"
+
+# Note: All code that uses this includes a test to check if the
+# variables are present, before referencing them. So don't worry
+# if you don't have values to fill them with.
+
+
+# The Verification Token provided in the first step of registration
+V_TOKEN = '' 
+# The Security Token provided in the first step of registration
+S_TOKEN = ''
+# Remember to remove these from code once you've completed registration
+
+# The Key & Secret given after registration is successful. 
+KEY = ''
+SECRET = ''
